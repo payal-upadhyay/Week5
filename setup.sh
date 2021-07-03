048a3cd3e02697881cbcebca5647e3db37720440
payal-upadhyay <85113311+payal-upadhyay@users.noreply.github.com>
2021-07-04 00:27:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 
+  
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"payalupadhyay@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
